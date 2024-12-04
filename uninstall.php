1c8a55b7547b7d5496d1edc660b46f73ccea6525
WP Speed Expert <139096428+WPSpeedExpert@users.noreply.github.com>
2024-12-04 02:02:44 +0100

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,8 @@
+<?php
+// Exit if accessed directly
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
+}
+
+// Perform any necessary cleanup here. For this plugin, no settings or custom database tables are created,
+// so no cleanup is needed.
